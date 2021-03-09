9b68387bc6e9a374c06a3b9d02819d061604f700
Summer Hasama <summerhasama@gmail.com>
2021-03-08 22:43:30 -0800

    add firebase to website, create save score functio

---
+++ public/script/myFirebase.js
@@ -0,0 +1,34 @@
+
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCX89JeiE2dcSVcDrcCgYqYE9DPlG1vMPA",
+    authDomain: "pomonawebgame.firebaseapp.com",
+    projectId: "pomonawebgame",
+    storageBucket: "pomonawebgame.appspot.com",
+    messagingSenderId: "1010804082816",
+    appId: "1:1010804082816:web:268bdc9990665bb6207e65",
+    measurementId: "G-2GYMK1ENK2"
+  };
+  
+firebase.initializeApp(firebaseConfig);
+var db = firebase.firestore();
+
+function saveScore(name,score,year){
+
+    db.collection("scores").add({
+        name: name,
+        score: score,
+        year: year
+    })
+    .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+        console.error("Error adding document: ", error);
+    });
+
+  }
+
+
+
